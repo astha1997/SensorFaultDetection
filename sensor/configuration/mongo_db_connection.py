17b1c107129c26afd761156fdde55feeed028d04
Astha Sachan <asthasachan0@gmail.com>
2023-07-21 15:45:32 +0530

    Mongodb

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,6 +1,6 @@
 import pymongo
 from sensor.constant.database import DATABASE_NAME
-from sensor.constant.env_variable import MONGODB_URL_KEY
+#from sensor.constant.env_variable import MONGODB_URL_KEY
 import certifi
 import os
 ca = certifi.where()
@@ -11,7 +11,7 @@ class MongoDBClient:
         try:
 
             if MongoDBClient.client is None:
-                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                mongo_db_url = "mongodb+srv://mongodb:mongodb@cluster0.bfgcgfc.mongodb.net/?retryWrites=true&w=majority"
                 print(mongo_db_url)
                 if "localhost" in mongo_db_url:
                     MongoDBClient.client = pymongo.MongoClient(mongo_db_url) 
